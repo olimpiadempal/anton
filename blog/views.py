d21c53314633baed83d9b26048ed53a13ab04b37
Taniyatj <Ghotra452@gmail.com>
2020-07-06 13:00:04 +0200

    Bild

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,31 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.utils import timezone
-from django.views.generic import ListView, CreateView
-from django.urls import reverse_lazy
 from .models import Post
+from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
 from django.db.models import Q
 
+
+
+
+
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     #return render(request, 'blog/post_list.html', {'posts': posts})
-    return render(request, 'blog/post_list.html', {'posts': posts})
+
+    category = request.GET.get('category')
+    if category:
+        posts = posts.filter(category=category)
+    return render(request, 'blog/post_list.html', {'posts': posts, 'category_choices': Post.CATEGORY_CHOICES })
+
+
+
+
+
+
+
 
     paginator = Paginator(queryset_list, 10)
     page_request_var = 'page'
@@ -22,22 +37,13 @@ def post_list(request):
     except EmptyPage:
         queryset = paginator.page(paginator.num_pages)
 
+
 def post_detail(request, pk):
     Post.objects.get(pk=pk)
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
-
-class View(ListView):
-    model = Post
-    template_name = 'post_list.html'
-
-class CreatePostView(CreateView): # new
-    model = Post
-    form_class = PostForm
-    template_name = 'post_edit.html'
-    success_url = reverse_lazy('post_list')
 
 def post_new(request):
     form = PostForm()
@@ -58,7 +64,6 @@ def post_new(request):
         form = PostForm()
 
     return render(request, 'blog/post_edit.html', {'form': form})
-
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
